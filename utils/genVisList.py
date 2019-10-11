52610d2cc536ff7be0f7c6db5933ba7dae0ef96a
Nickolas, Pingel (ANU) <Nickolas.Pingel@anu.edu.au>
2019-10-11 11:58:35 +1100

    initial modifications

--- utils/genVisList.py
+++ utils/genVisList.py
@@ -1,4 +1,17 @@
-## script to generate a list of visibilities for CASA task input
+"""
+10/11/2019
+Script to generate a list of all MS files associated with a given GASKAP field. The aim to be generic enough
+such that one only needs to change the schedule block ID, interleave name, and master path to the calibrated
+visibilities. No user inputs. 
+
+__author__="Nickolas Pingel"
+__version__="1.0"
+__email__="Nickolas.Pingel@anu.edu.au"
+__status__="Production"
+"""
+
+
+
 SBArr = ['4567', '4570', '4577']
 for SB in SBArr:
 	for i in range(0, 36):

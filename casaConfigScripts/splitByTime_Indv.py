548577b7b6e049914b3b225d1957cca340be06e7
Nickolas, Pingel (ANU) <Nickolas.Pingel@anu.edu.au>
2019-11-28 16:13:28 +1100

    add ability to skip processing of specific beams/interleaves in pbs scripts; create splitByTime casa config script

---
+++ casaConfigScripts/splitByTime_Indv.py
@@ -0,0 +1,35 @@
+"""
+11/28/2019
+Split input ms by input time range string
+User inputs:
+-t --timeRange -<required> time string over which to split out the visibiltities; (i.e., YYYY/MM/DD/hh:mm:ss~YYYY/MM/DD/hh:mm:ss)
+-n --msPath -<required> FULL path of ms file
+
+__author__="Nickolas Pingel"
+__version__="1.0"
+__email__="Nickolas.Pingel@anu.edu.au"
+__status__="Production"
+"""
+# imports
+import argparse
+
+## parse user inputs
+parser = argparse.ArgumentParser()
+parser.add_argument('-t', '--timeRange', help='<required> time string over which to split out the visibiltities; (i.e., YYYY/MM/DD/hh:mm:ss~YYYY/MM/DD/hh:mm:ss)', required = True)
+parser.add_argument('-n', '--msPath', help = '<required> FULL path of ms file', required = True)
+args, unknown = parser.parse_known_args()
+
+## get number of channels to use in average
+timeRange = args.time
+
+## parse name of visibiltiy from input path
+msName = args.msPath.split('/')[-1]
+
+## set split parameters and run
+default('split')
+print('Splitting out %s from %s ' % (timeRange, msName))
+vis = args.msPath
+outputvis = args.msPath + '.timeSplit'
+timerange = timeRange
+datacolumn = 'data'
+split()

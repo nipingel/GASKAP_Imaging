f0b7699e3a9da3d9415866c3a4fb2a26c66ad89c
Nickolas, Pingel (ANU) <Nickolas.Pingel@anu.edu.au>
2020-06-16 19:17:55 +1000

    add scheduling script to concat two ms files

--- casaConfigScripts/concatVis.py
+++ casaConfigScripts/concatVis.py
@@ -4,7 +4,7 @@ script to configure the CASA task 'concat' to merge visibilities of the same bea
 User inputs:
 -p1 --path1 -<required> FULL path to first ms file
 -p2 --path2 -<required> FULL path of second ms file
--c --concat_path -<required> FULL path for concatenated ms file
+-o --output_vis -<required> FULL path for concatenated ms file
 
 __author__="Nickolas Pingel"
 __version__="1.0"
@@ -18,7 +18,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-p1', '--path1', help='<required> FULL path to first ms file', required = True)
 parser.add_argument('-p2', '--path2', help = '<required> FULL path to second ms file', required = True)
-parser.add_argument('-c', '--concat_path', help = '<required> FULL path for concatenated msfile' , required = True)
+parser.add_argument('-o', '--output_vis', help = '<required> FULL path for concatenated msfile' , required = True)
 args, unknown = parser.parse_known_args()
 
 ## parse name of first path to ms file
@@ -35,6 +35,6 @@ visList.extend([args.path1, args2.path2])
 default('concat')
 print('Concatenating %s and %s channels' % (msName_1, msName_2))
 vis = visList
-concatvis = args.concat_path
+concatvis = args.output_vis
 copypointing = False
 concat()

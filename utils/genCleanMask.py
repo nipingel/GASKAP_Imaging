885afdbadaeb4790dda6995d63bbed18e75507b7
Nickolas, Pingel (ANU) <Nickolas.Pingel@anu.edu.au>
2021-03-18 12:47:32 +1100

    include absorbed pixels in clean mask generated with utils/genCleanMask.py

--- utils/genCleanMask.py
+++ utils/genCleanMask.py
@@ -1,14 +1,15 @@
 """
-04/01/2020
-Script to generate a clean mask from list of files from a user provided extension (e.g., '.image') and threshold level, 
-which denotes the minimum pixel value to be included in the clean mask. Generally ~2x the noise
+03/18/2021
+Script to generate a clean mask from list of files from a user provided extension (e.g., '.image') and postive/negative threshold level, 
+which denotes the minimum absorbed and emission pixel values to be included in the clean mask. Generally ~2x the noise
 These masks will then be used by tclean to continue cleaning/imaging. 
 
 ** MUST BE RUN IN SAME DIRECTORY AS INPUT FILES **
 
 User inputs:
 -f --fileExt - <required> file extension (e.g., 'image')
--t --threshold - <required> minimum pixel value to include in mask 
+-t --threshold - <required> minimum emission pixel value to include in mask 
+-n --abs_threshold - <optional> minimum absorbed piixel value to include in mask
 
 __author__="Nickolas Pingel"
 __email__="Nickolas.Pingel@anu.edu.au"
@@ -22,13 +23,20 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--fileExt', help = '<required> file extension (e.g., image)', required = True)
 parser.add_argument('-t', '--threshold', help = '<required> minimum pixel value to include in mask', required = True)
+parser.add_argument('-n', '--abs_threshold', help = '<optional>minimum absorbed piixel value to include in mask', required = True)
+
 
 args, unknown = parser.parse_known_args()
 
 fileExt = args.fileExt
 threshold = args.threshold
+abs_threshold = args.abs_threshold
 
-print('Creating mask from pixels with values at and above %s [Jy/Beam]' % threshold)
+if abs_threshold is not None:
+	print('Creating mask from emission pixels with values at and above %s [Jy/Beam]' % threshold)
+	print('Including absorbed pixels with values at and below %s [Jy/Beam]' % abs_threshold)
+else:
+	print('Creating mask from just emission pixels with values at and above %s [Jy/Beam]' % threshold)
 
 ## create list of files
 fileList = glob.glob('*.%s' % fileExt)
@@ -40,10 +48,16 @@ fileList.sort()
 for file in fileList:
 	print('Generating mask for file: %s' % file)
 
-	## make internal mask based on threshold
-	ia.open(file)
-	ia.calcmask(mask = '%s > %s' % (file, threshold), name = 'sig_2.0.mask')
-	ia.done()
+	if abs_threshold is not None:
+		## make internal mask based on absorption/emission thresholds
+		ia.open(file)
+		ia.calcmask(mask = '%s > %s || %s < %s' % (file, threshold, file, abs_threshold), name = 'sig_2.0.mask')
+		ia.done()
+	else:
+		## make internal mask based on absorption/emission thresholds
+		ia.open(file)
+		ia.calcmask(mask = '%s > %s' % (file, threshold), name = 'sig_2.0.mask')
+		ia.done()
 
 	## generate actual 1/0 mask image
 	default('makemask')

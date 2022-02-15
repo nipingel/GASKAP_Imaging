117e6786484fac2b573305f08c935078a018d54d
Nickolas, Pingel (ANU) <Nickolas.Pingel@anu.edu.au>
2022-02-15 12:08:44 +1100

    create lists of measurement set in casa script so only a single instance of casa is launched

--- casaConfigScripts/splitByChannel_Indv.py
+++ casaConfigScripts/splitByChannel_Indv.py
@@ -2,8 +2,10 @@
 11/20/2019
 Split out a user provided channel fromj input ms file
 User inputs:
--n --msPath -<required> FULL path of ms file
--c --chanNum -<required> channel to split out
+-p --path - <required> path of ms file
+-n --name - <required> name of measurement set
+-s --start_chan - <required> starting channel for splitting
+-e --end_chan - <required> ending channel for splliting
 __author__="Nickolas Pingel"
 __version__="1.0"
 __email__="Nickolas.Pingel@anu.edu.au"
@@ -14,24 +16,34 @@ import argparse
 
 ## parse user inputs
 parser = argparse.ArgumentParser()
-parser.add_argument('-n', '--msPath', help = '<required> FULL path of ms file', required = True)
-parser.add_argument('-c', '--chanNum', help='<required> channel to split out', required = True)
+parser.add_argument('-p', '--path', help = '<required> path to measurement set', required = True)
+parser.add_argument('-n', '--name', help='<required> name of measurement set', required = True)
+parser.add_argument('-s', '--start_chan', help='<required> starting channel for splitting', required = True, type = int)
+parser.add_argument('-e', '--end_chan', help='<required> ending channel for splitting', required = True, type = int)
 args, unknown = parser.parse_known_args()
 
-## get number of channels to use in average
-chanNum = args.chanNum
+## get path to measurement set
+ms_path = args.path
 
-## parse name of visibiltiy from input path
-msName = args.msPath.split('/')[-1]
+## get name of measurement set
+ms_name = args.name 
 
-## determine file suffix
-fileSuffix = msName.split('.')[-1]
+## get starting and ending channels
+start_chan = args.start_chan
+end_chan = args.end_chan
 
-## set split parameters and run
-default('split')
-print('Splitting out channel %s from %s ' % (chanNum, msName))
-vis = args.msPath
-outputvis = args.msPath + '_chan%s.ms' % chanNum
-spw='0:%s' % chanNum
-datacolumn = 'data'
-split()
+## create list of file names
+output_vis_list = []
+for i in range(start_chan, end_chan):
+	output_vis_list.append('%s/%s_chan%d' % (path, name, i))
+
+cnt = 0
+for i in range(start_chan, end_chan+1):
+	## set split parameters and run
+	default('split')
+	vis = '%s/%s' % (ms_path, ms_name)
+	outputvis = output_vis_list[cnt]
+	spw='0:%d' % i
+	datacolumn = 'data'
+	cnt+=1
+	split()

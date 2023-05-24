21804173bd038f9a359a62da658e130122c99824
Nickolas Pingel <nickolaspingel@Nickolass-MacBook-Pro-2.local>
2023-05-24 14:16:24 -0500

    do not overwrite beam image FITS file when moving back to staging area

--- chtc_scripts/untar_images.sh
+++ chtc_scripts/untar_images.sh
@@ -8,23 +8,26 @@ file_suffix=$1
 root_file_name=$2
 sbid=$3
 chan=$4
-output_name=${root_file_name}_chan${chan}-${file_suffix}.fits
+image_output_name=${root_file_name}_chan${chan}-${file_suffix}.fits
+beam_output_name=${root_file_name}_chan${chan}-beam.fits
 
 ## check if 0 needs to be appended in name (channel range from 0 to 99) for alphanumeric ordering
 if [ "${chan}" -lt "10" ]; then
-        output_name=${root_file_name}_chan00${chan}.${file_suffix}.fits
+        image_output_name=${root_file_name}_chan00${chan}-${file_suffix}.fits
+        beam_output_name=${root_file_name}_chan00${chan}-beam.fits
 fi
 ## check if 0 needs to be appended in name (channel range from 100 to 999) for alphanumeric ordering
 if [ "${chan}" -lt "100" ] && [ "${chan}" -gt "9" ]; then
-        output_name=${root_file_name}_chan0${chan}.${file_suffix}.fits
+        image_output_name=${root_file_name}_chan0${chan}-${file_suffix}.fits
+        beam_output_name=${root_file_name}_chan00${chan}-beam.fits
 fi
 
 ## untar
 tar -xvf /projects/vla-processing/GASKAP-HI/images/${sbid}/mw_velocities/${root_file_name}_chan${chan}.tar --directory .
 
 ## move image and pb fits files to designated directory
-mv ${root_file_name}_chan${chan}-${file_suffix}.fits /projects/vla-processing/GASKAP-HI/images/${sbid}/mw_velocities/${output_name}
-mv ${root_file_name}_chan${chan}-beam.fits /projects/vla-processing/GASKAP-HI/images/${sbid}/mw_velocities/${output_name}
+mv ${root_file_name}_chan${chan}-${file_suffix}.fits /projects/vla-processing/GASKAP-HI/images/${sbid}/mw_velocities/${image_output_name}
+mv ${root_file_name}_chan${chan}-beam.fits /projects/vla-processing/GASKAP-HI/images/${sbid}/mw_velocities/${beam_output_name}
 
 ## clean up
 rm -rf ${root_file_name}_chan${chan}*

89bd037f8bdcc47c238140dbb470758572c0d5a6
Nickolas Pingel <nickolaspingel@Nickolass-MacBook-Pro-2.local>
2023-06-20 19:02:23 -0500

    set up split_channels node

--- chtc_scripts/subregions_DAG/split_channels.sh
+++ chtc_scripts/subregions_DAG/split_channels.sh
@@ -12,18 +12,14 @@ start_chan=$3
 end_chan=$4
 
 ## untar measurement set to working directory
-tar -xvf /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/${ms_file}.contsub --directory .
+cd /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 
 # make casa call to imaging script
-/casa-6.5.0-15-py3.8/bin/casa -c split_channels.py -p ${ms_file}.contsub -s ${start_chan} -e ${end_chan}
+/casa-6.5.0-15-py3.8/bin/casa -c split_channels.py -p ${ms_file} -s ${start_chan} -e ${end_chan}
 
 ## loop through to tar each split out file
 for ((i=$2; i<=$3; i++))
 do
-	tar -cvf ${ms_file}.contsub_chan${i}.tar ${ms_file}.contsub_chan${i}
-	mv ${ms_file}.contsub_chan${i}.tar /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}
+	tar -cvf ${ms_file}_chan${i}.tar ${ms_file}_chan${i}
+	#mv ${ms_file}.contsub_chan${i}.tar /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 done
-
-## clean up
-rm -rf ${ms_file}.contsub
-rm -rf ${ms_file}.contsub_chan*

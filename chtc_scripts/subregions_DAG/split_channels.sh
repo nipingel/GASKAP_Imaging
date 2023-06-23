27453e64114b78fddc52317d71d749b643fd9e6d
Nickolas Pingel <nickolaspingel@Nickolass-MacBook-Pro-2.local>
2023-06-22 20:35:27 -0500

    add tar measurement sets node

--- chtc_scripts/subregions_DAG/split_channels.sh
+++ chtc_scripts/subregions_DAG/split_channels.sh
@@ -12,8 +12,7 @@ start_chan=$3
 end_chan=$4
 
 ## untar measurement set to working directory
-mv split_channels.py /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
-cd /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
+tar -xvf /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor/${ms_file}.tar --directory .
 
 # make casa call to imaging script
 /casa-6.5.0-15-py3.8/bin/casa -c split_channels.py -p ${ms_file} -s ${start_chan} -e ${end_chan}
@@ -22,7 +21,9 @@ cd /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 for ((i=$3; i<=$4; i++))
 do
 	tar -cvf ${ms_file}_chan${i}.tar ${ms_file}_chan${i}
-	#mv ${ms_file}.contsub_chan${i}.tar /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
+	mv ${ms_file}_chan${i}.tar /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 done
 
+## clean up
+rm -rf ${ms_file}*
 rm split_channels.py
---
+++ chtc_scripts/subregions_DAG/tar_measurement_sets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# untar_measurement_sets.sh
+# execution script to untar input measurement set 
+
+sbid=$1
+file_name=$2
+
+cd /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
+
+## untar 
+tar -cvf ${file_name}

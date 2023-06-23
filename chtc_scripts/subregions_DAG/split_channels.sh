85c5ad77f6b34cb7440536284022acd2b0e32fe0
nipingel <nmpingel@wisc.edu>
2023-06-22 20:18:49 -0500

    edits to split_channels

--- chtc_scripts/subregions_DAG/split_channels.sh
+++ chtc_scripts/subregions_DAG/split_channels.sh
@@ -12,14 +12,17 @@ start_chan=$3
 end_chan=$4
 
 ## untar measurement set to working directory
+mv split_channels.py /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 cd /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 
 # make casa call to imaging script
 /casa-6.5.0-15-py3.8/bin/casa -c split_channels.py -p ${ms_file} -s ${start_chan} -e ${end_chan}
 
 ## loop through to tar each split out file
-for ((i=$2; i<=$3; i++))
+for ((i=$3; i<=$4; i++))
 do
 	tar -cvf ${ms_file}_chan${i}.tar ${ms_file}_chan${i}
 	#mv ${ms_file}.contsub_chan${i}.tar /projects/vla-processing/GASKAP-HI/measurement_sets/${sbid}/30Dor
 done
+
+rm split_channels.py
